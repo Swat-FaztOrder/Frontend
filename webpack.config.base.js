c820dadd8e668feeddc7b5dc8050b72fc0e97a3d
Xavier Garcia <xaviergn60@gmail.com>
2020-11-18 22:11:13 -0700

    added some configurations and npm packages

---
+++ webpack.config.base.js
@@ -0,0 +1,44 @@
+//import webpack plugins
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+//css rules
+const cssRules = {
+  test: /\.styl$/i,
+  use: ["style-loader", "css-loader", "stylus-loader"],
+  exclude: /node_modules/,
+};
+
+//images rules
+const imgRules = {
+  test: /\.(jpg|png|svg)$/,
+  use: {
+    loader: "url-loader",
+    options: {
+      limit: 90000,
+    },
+  },
+};
+
+//babel rules
+const babelRules = {
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  loader: "babel-loader",
+};
+
+//export config
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    filename: "bundle.[contentHash].js",
+  },
+  module: {
+    rules: [babelRules, cssRules, imgRules],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: "FastOrder",
+      template: "./public/index.html",
+    }),
+  ],
+};
