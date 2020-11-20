06160f0b98116f0bb2679f68934c5b4e29e70af8
Xavier Garcia <xaviergn60@gmail.com>
2020-11-20 13:54:10 -0700

    added the global styles and the pages folder

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 import '@babel/polyfill';
 import App from './components/App/index.jsx';
 
+import './stylus/globalStyles.styl'
+
 // create root const
 const root = document.getElementById('root');
 
