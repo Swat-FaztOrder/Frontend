f084aaa0db1ab88585b1678297906dbd0616a466
Xavier Garcia <xaviergn60@gmail.com>
2020-11-23 01:45:41 -0700

    added a header

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ItemDetails from '../../components/ItemDetails/index.jsx';
 import MenuGrid from '../../components/MenuGrid/index.jsx';
+import Header from '../../components/Header/index.jsx'
 
 import './styles.styl'
 
 const Menu = () => {
   return (
     <div className="Menu">
+      <Header/>
       <MenuGrid />
       <ItemDetails />
     </div>
