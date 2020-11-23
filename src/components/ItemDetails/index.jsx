1b84ed80cda5e63040a964a74c5a377598b24bc8
Xavier Garcia <xaviergn60@gmail.com>
2020-11-22 17:57:10 -0700

    ended layout for itemDetails, now it needs styles

--- src/components/ItemDetails/index.jsx
+++ src/components/ItemDetails/index.jsx
@@ -4,15 +4,54 @@ import './styles.styl';
 
 const ItemDetails = ({ extras, image, title, details, price }) => {
   return (
-    <div className="ItemDetails">
-      <img className="ItemDetails__image" src="" alt="" />
-      <h2 className="ItemDetails__title">{title}</h2>
-      <div className="ItemDetails__subtitle">
+    <div className="itemDetails">
+      <img className="itemDetails__image" src="https://i.imgur.com/gfMP09b.jpg" alt="" />
+      <i className="fas fa-arrow-circle-left" id="itemDetails__arrow" />
+      <h2 className="itemDetails__title">{title}</h2>
+      <div className="itemDetails__subtitle">
         <p>{details}</p>
         <h3>{price}</h3>
       </div>
       <span>ADD OPTIONS</span>
-      <i className="fab fa-angular"></i>
+      <div className="itemDetails__extras">
+        <div className="itemDetails__extras--line">
+          <div className="itemDetails__extras--lineLeft">
+            <input type="checkbox" id="cbox1" />
+            <p>Extra Cheese</p>
+          </div>
+          <p className="itemDetails__extras--lineRight">$5.99</p>
+        </div>
+        <div className="itemDetails__extras--line">
+          <div className="itemDetails__extras--lineLeft">
+            <input type="checkbox" id="cbox2" />
+            <p>Extra Cheese</p>
+          </div>
+          <p className="itemDetails__extras--lineRight">$5.99</p>
+        </div>
+        <div className="itemDetails__extras--line">
+          <div className="itemDetails__extras--lineLeft">
+            <input type="checkbox" id="cbox3" />
+            <p>Extra Cheese</p>
+          </div>
+          <p className="itemDetails__extras--lineRight">$5.99</p>
+        </div>
+        <div className="itemDetails__extras--line">
+          <div className="itemDetails__extras--lineLeft">
+            <input type="checkbox" id="cbox4" />
+            <p>Extra Cheese</p>
+          </div>
+          <p className="itemDetails__extras--lineRight">$5.99</p>
+        </div>
+      </div>
+      <input
+        type="text"
+        placeholder="Extra info..."
+        id=""
+        className="itemDetails__inputText"
+      />
+      <button className="itemDetails__addOrder">
+        Add to order
+      </button>
     </div>
   )
 }
