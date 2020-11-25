4e24ab46c46982e30382ba7586117b3d976a42cd
Xavier Garcia <xaviergn60@gmail.com>
2020-11-25 14:07:21 -0700

    typo fix and npm i

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from '../../pages/login/index.jsx';
+import Login from '../../pages/Login/index.jsx';
 import Menu from '../../pages/Menu/index.jsx'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/login"><Login /></Route>
+        <Route exact path="/Login"><Login /></Route>
         <Route exact path="/Menu"><Menu/></Route>
       </Switch>
     </Router>
