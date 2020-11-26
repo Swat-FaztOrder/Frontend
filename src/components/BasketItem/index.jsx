0b48506fdd748ae0bcd287f52ffaa6cd13ffe3d4
Xavier Garcia <xaviergn60@gmail.com>
2020-11-25 18:53:11 -0700

    basket components ready

--- src/components/BasketItem/index.jsx
+++ src/components/BasketItem/index.jsx
@@ -5,12 +5,14 @@ import './styles.styl'
 const BasketItem = ({ price, quantity, title, image }) => {
   return (
     <div className="basketItem">
-      <img src={image} alt="" className="basketItem--image" />
-      <div className="basketItem--details">
+      <div className="basketItem__image">
+        <img src={image} alt="" />
+      </div>
+      <div className="basketItem__details">
         <h2>{title}</h2>
         <span className="quantity">x{quantity}</span>
       </div>
-      <h2 className="basketItem--price">${price}</h2>
+      <h2 className="basketItem__price">${price}</h2>
     </div>
   )
 }
