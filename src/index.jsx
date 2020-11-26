02151d53555c0c31bb7145910eda3a547a52f404
Xavier Garcia <xaviergn60@gmail.com>
2020-11-26 16:14:31 -0700

    Revert "Feature/basket modal"

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,10 @@
-import React, { Suspense } from 'react'
-import { render } from 'react-dom'
-
+import React from 'react';
+import ReactDom from 'react-dom';
 import '@babel/polyfill';
-
-/* i18n  */
-import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n'
-
 import App from './components/App/index.jsx';
-
-import './stylus/globalStyles.styl';
 
 // create root const
 const root = document.getElementById('root');
 
 // render the app
-render(
-  <Suspense fallback="loading">
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </Suspense>,
-  root
-)
+ReactDom.render(<App />, root);
