f98fbd7a5b0e78293e139384917864ae415b9247
Julian Cubillos <juliandavid25@hotmail.com>
2020-11-26 22:02:33 -0500

    Feat update people view

--- src/pages/Profiles/index.jsx
+++ src/pages/Profiles/index.jsx
@@ -3,7 +3,7 @@ import ItemDetails from '../../components/ItemDetails/index.jsx';
 import PeopleGrid from '../../components/PeopleGrid/index.jsx';
 import Header from '../../components/Header/index.jsx'
 import MenuCategories from '../../components/MenuCategories/index.jsx'
-import OrderDetails from '../../components/OrderDetails/index.jsx';
+import PeopleDetails from '../../components/PeopleDetails/index.jsx';
 
 import './styles.styl'
 
@@ -13,15 +13,14 @@ const Menu = () => {
       <Header />
       <main className="profile__container">
         <section className="profile__container--left">
-          <MenuCategories/>
+          <MenuCategories />
         </section>
         <section className="profile__container--center">
           <PeopleGrid />
         </section>
         <section className="profile__container--right">
-          <OrderDetails/>
+          <PeopleDetails />
         </section>
-        <ItemDetails details="Lorem Ipsum" price="5.99" title="Lorem Ipsum" subtitle="Lorem Ipsum"/>
       </main>
     </div>
   )
