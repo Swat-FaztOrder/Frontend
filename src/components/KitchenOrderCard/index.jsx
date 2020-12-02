e04e40b083ec843b978d4bd4f4b7480a90206018
Xavier Garcia <xaviergn60@gmail.com>
2020-12-02 16:37:15 -0700

    style: basic css styles for kitchenordercard done

--- src/components/KitchenOrderCard/index.jsx
+++ src/components/KitchenOrderCard/index.jsx
@@ -4,17 +4,20 @@ import './styles.styl'
 const KitchenOrderCard = ({ tableNum, items }) => {
   return (
     <div className="orderCard">
-      <div className="orderCard__table">
+      <div className="orderCard__table redStat">
         <h1>Table {tableNum}</h1>
       </div>
       <div className="orderCard__list">
         {items.map((item, i) => (
           <div className="orderCard__list--item" key={i}>
-            <h3>{item.quantity}</h3>
-            <h3>{item.title}</h3>
+            <h3 className="orderCard__list--itemQuantity">{item.quantity}</h3>
+            <h3 className="orderCard__list--itemTitle">{item.title}</h3>
           </div>
         ))}
       </div>
+      <a href="" className="orderCard__icon">
+        <i className="fas fa-sticky-note" />
+      </a>
     </div>
   )
 }
--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -5,19 +5,54 @@ import KitchenOrderCard from '../../components/KitchenOrderCard/index.jsx'
 import './styles.styl'
 
 const Kitchen = () => {
-  const itemList = [
+  const itemList1 = [
     { title: 'Hamburguer', quantity: 1 },
     { title: 'Pizza', quantity: 1 },
     { title: 'Beverage', quantity: 2 },
     { title: 'Hotdog', quantity: 1 },
     { title: 'Subway', quantity: 1 }
   ]
+  const itemList2 = [
+    { title: 'Hamburguer', quantity: 1 },
+    { title: 'Pizza', quantity: 1 },
+    { title: 'Beverage', quantity: 2 },
+    { title: 'Hotdog', quantity: 1 },
+    { title: 'Beverage', quantity: 2 },
+    { title: 'Hotdog', quantity: 1 },
+    { title: 'Beverage', quantity: 2 },
+    { title: 'Hotdog', quantity: 1 },
+    { title: 'Beverage', quantity: 2 },
+    { title: 'Hotdog', quantity: 1 },
+    { title: 'Subway', quantity: 1 }
+  ]
+  const itemList3 = [
+    { title: 'Hamburguer', quantity: 1 },
+    { title: 'Pizza', quantity: 1 },
+    { title: 'Beverage', quantity: 2 },
+    { title: 'Hotdog', quantity: 1 },
+    { title: 'Subway', quantity: 1 },
+    { title: 'Hamburguer', quantity: 1 },
+    { title: 'Pizza', quantity: 1 },
+    { title: 'Beverage', quantity: 2 },
+    { title: 'Hotdog', quantity: 1 },
+    { title: 'Subway', quantity: 1 },
+    { title: 'Hamburguer', quantity: 1 },
+    { title: 'Pizza', quantity: 1 },
+    { title: 'Beverage', quantity: 2 }
+  ]
 
   return (
     <div className="kitchen">
       <Header />
       <main className="orders">
-        <KitchenOrderCard tableNum="1" items={itemList} />
+        <KitchenOrderCard tableNum="1" items={itemList1} />
+        <KitchenOrderCard tableNum="2" items={itemList2} />
+        <KitchenOrderCard tableNum="3" items={itemList3} />
+        <KitchenOrderCard tableNum="4" items={itemList1} />
+        {/* <KitchenOrderCard tableNum="5" items={itemList2} />
+        <KitchenOrderCard tableNum="5" items={itemList3} />
+        <KitchenOrderCard tableNum="6" items={itemList1} />
+        <KitchenOrderCard tableNum="7" items={itemList2} /> */}
       </main>
     </div>
   )
