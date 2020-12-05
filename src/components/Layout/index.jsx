d7064a68db955e0155e3bc4a05396f437ac4fd9a
digitros <dgogo9@hotmail.com>
2020-12-04 18:40:50 -0600

    [FIX] Kitchen view now has the layout implemented

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 /* Components */
 import Header from '../Header/index.jsx'
-import ItemDetails from '../ItemDetails/index.jsx';
 import MenuCategories from '../MenuCategories/index.jsx';
 import OrderDetails from '../OrderDetails/index.jsx';
+import constants from '../constants/routes'
 
 /* Hooks */
 import { useLocation } from 'react-router-dom'
@@ -13,14 +13,16 @@ import { useLocation } from 'react-router-dom'
 import './styles.styl'
 
 const index = ({ children }) => {
-  const HeaderComponent = useLocation().pathname !== '/signin' ? <Header/> : ''
-  const MenuComponent = useLocation().pathname !== '/signin' ? <MenuCategories/> : ''
-  const OrderComponent = useLocation().pathname !== '/signin' && <OrderDetails/>
+  const location = useLocation().pathname
+
+  const HeaderComponent = location !== constants.SIGN_IN ? <Header/> : ''
+  const MenuComponent = location !== constants.SIGN_IN && location !== constants.KITCHEN ? <MenuCategories/> : ''
+  const OrderComponent = location !== constants.SIGN_IN && location !== constants.KITCHEN ? <OrderDetails/> : ''
 
   return (
     <>
       {HeaderComponent}
-      <div className={`Main${useLocation().pathname !== '/signin' ? '' : '--Login'}`}>
+      <div className={`Main${location !== '/signin' && location !== constants.KITCHEN ? '' : '--Login'}`}>
         {MenuComponent}
         {children}
         {OrderComponent}
