da9406517553dbcae5f13057854d1cae9e16634d
Xavier Garcia <xaviergn60@gmail.com>
2020-12-06 14:37:21 -0700

    style: added the modals for menu

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './styles.styl'
+
+const Modal = ({ title, image, subtitleA, subtitleB, last, buttons, buttonA, buttonB }) => {
+  return (
+    <section className="modalContainer">
+      <main className="modalContainer__modal">
+        <div className="modalContainer__modal--title">
+          <h1>{title}</h1>
+        </div>
+        <div className="modalContainer__modal--image">
+          <img src={image} alt="" />
+        </div>
+        <div className="modalContainer__modal--subtitle">
+          <h3>{subtitleA}</h3>
+          <h3>{subtitleB}</h3>
+        </div>
+        <div className="modalContainer__modal--lastMessage">
+          <h2>{last}</h2>
+        </div>
+        {buttons === 'true' &&
+          <div className="modalContainer__modal--buttons">
+            <button className="modalContainer__modal--buttons-1">
+              {buttonA}
+            </button>
+            <button className="modalContainer__modal--buttons-2">
+              {buttonB}
+            </button>
+          </div>
+        }
+      </main>
+    </section>
+  )
+}
+
+export default Modal
