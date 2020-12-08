9a86b29188b0e3d347950a96aa8e2e0cc6480c05
Julian Cubillos <juliandavid25@hotmail.com>
2020-12-08 13:04:37 -0500

    Fix change admin modals view and responsive views

--- src/pages/Profiles/index.jsx
+++ src/pages/Profiles/index.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 /* Components */
 import PeopleGrid from '../../components/PeopleGrid/index.jsx';
+import Modal from '../../components/Modal/index.jsx'
+import Waiter from '../../assets/waiter.png'
 
 /* Styles */
 import './styles.styl'
 
 const Menu = () => {
   return (
-    <PeopleGrid />
+    <>
+      <PeopleGrid />
+      {<Modal title="Added User!!!" image="https://images.pexels.com/photos/4871397/pexels-photo-4871397.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" subtitleA="Diego Valdez" subtitleB="Cheff" last="Your added user will be available to login right now" buttons="false"/>}
+      {<Modal title="Updated User!!!" image="https://images.pexels.com/photos/4871397/pexels-photo-4871397.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" subtitleA="Diego Valdez" subtitleB="Cheff" last="Your updated user will be available to login right now" buttons="false"/>}
+      {<Modal title="Deleted User!!!" image={Waiter} last="The user has been deleted" buttons="false"/>}
+    </>
   )
 }
 
