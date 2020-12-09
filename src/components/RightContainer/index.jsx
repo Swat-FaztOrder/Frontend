212c71630e10d9033a023de24678fed9e5ca59da
Xavier Garcia <xaviergn60@gmail.com>
2020-12-09 10:22:24 -0700

    style: created "CategoriesRight" component

--- src/components/RightContainer/index.jsx
+++ src/components/RightContainer/index.jsx
@@ -23,26 +23,6 @@ const RightContainer = () => {
           title2="Platzi Master"
         />
       }
-      {actionLayout === ActionTypes.PROFILE_ADD &&
-        <OrderDetails
-          image={waiter}
-          Button="false"
-          subtitle1="Admin"
-          title1="Diego Valdez"
-          subtitle2="Restaurant"
-          title2="Platzi Master"
-        />
-      }
-      {actionLayout === ActionTypes.PROFILE_UPDATE &&
-        <OrderDetails
-          image={waiter}
-          Button="false"
-          subtitle1="Admin"
-          title1="Diego Valdez"
-          subtitle2="Restaurant"
-          title2="Platzi Master"
-        />
-      }
     </div>
   )
 }
