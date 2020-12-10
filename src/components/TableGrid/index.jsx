3bdd265efab3846bdd2737c21c7962f311283c5a
digitros <dgogo9@hotmail.com>
2020-12-10 13:58:38 -0600

    Show ADD and DELETE buttons just for the Admin

--- src/components/TableGrid/index.jsx
+++ src/components/TableGrid/index.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import TableCard from '../TableCard/index.jsx'
 
+/* Constants */
+import { USER } from '../../utils/constants/itemsLocalStorage'
+
 /* Styles */
 import './styles.styl';
 
@@ -19,6 +22,7 @@ import tableService from '../../services/table'
 const TableGrid = () => {
   const [tables, setTables] = useState([]);
   const [change, setChange] = useState(false)
+  const [role, setRole] = useState(JSON.parse(window.localStorage.getItem(USER)).role)
 
   const checkTableAvailability = () => {
     const table = tables.filter(item => !item.isActive).shift()
@@ -32,44 +36,25 @@ const TableGrid = () => {
   }
 
   const handleClick = (action) => {
-    console.log('Entro')
     if (action === 'ADD') {
       checkTableAvailability()
-      console.log('ADD')
     } else {
       const table = tables.filter((item) => item.isActive).pop()
-      // setTables(tables.map((item) => {
-      //   return item !== table
-      // }))
-      // console.log(table)
       tableService.update(table.id, table.name, false)
         .then(() => setChange(!change))
-      console.log('DELETE')
     }
 
   }
 
-  // useEffect(() => {
-  //   tableService.getAll()
-  //     .then(data => setTables(data.filter((item) => item.isActive)))
-  // }, [change])
-
   useEffect(() => {
     tableService.getAll()
       .then(data => {
-        console.log(data)
         setTables(data.sort((a, b) => (a.id - b.id)))
       })
-      // .then(() => {
-      //   setTables(tablesDB.filter((item) => item.isActive))
-      //   console.log(tablesDB)
-      // })
-      // .then(() => console.log(tablesDB))
   }, [change])
 
   const tablesList = tables.map((table) => {
     if (table?.isActive) {
-      //console.log(table.id, table.name)
       return (
         <TableCard key={table.id} title={`${table.name}`} state="No order" />
       )
@@ -80,8 +65,13 @@ const TableGrid = () => {
     <>
       <div className="TableGrid">
         {tablesList}
-        <Button onClick={() => handleClick('ADD')} type="Add table"><i className="fas fa-plus"/></Button>
-        <Button onClick={() => handleClick('DELETE')} type="Del table"><i className="fas fa-times"/></Button>
+        {role === 'admin' &&
+        <>
+          <Button onClick={() => handleClick('ADD')} type="Add table"><i className="fas fa-plus"/></Button>
+          <Button onClick={() => handleClick('DELETE')} type="Del table"><i className="fas fa-times"/></Button>
+        </>
+        }
+
       </div>
     </>
   )
