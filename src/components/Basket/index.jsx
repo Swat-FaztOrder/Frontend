05ac66a16ca71d5ef22747d290fc77d813e57805
digitros <dgogo9@hotmail.com>
2020-12-13 20:00:05 -0600

    [Feature] Basket updates itself.

--- src/components/Basket/index.jsx
+++ src/components/Basket/index.jsx
@@ -15,15 +15,18 @@ const Basket = () => {
   const { order, updateAction, ActionTypes } = useContext(Context)
   const [dishes, setDishes] = useState([])
   const [dishess, setDishess] = useState([])
+  const [change, setChange] = useState(false)
 
   const handleClick = () => {
+
     return orderService.send(order)
+      .then(() => setChange(!change))
   }
 
   useEffect(() => {
     orderDetailsService.getAll(order)
       .then(data => setDishess(data))
-  }, [order]);
+  }, [change]);
 
   useEffect(() => {
     const usedItemList = []
@@ -35,19 +38,14 @@ const Basket = () => {
           count++
         }
       })
-      console.log(count)
-      if (!usedItemList.includes([item.dish.id, item.cycleInKitchen])) {
-        usedItemList.push([item.dish.id, item.cycleInKitchen])
+      if (!usedItemList.includes(item.dish.id)) {
+        usedItemList.push(item.dish.id)
         return { id: item.id, price: item.dish.price, quantity: count, title: item.dish.name, image: item.dish.imageUrl, status: item.status }
       }
     })
-    console.log('used', usedItemList)
 
     setDishes(dish)
   }, [dishess]);
-
-  console.log('dishes', dishes)
-  console.log('dishess', dishess)
 
   const dishesList = dishes.length > 0 ? dishes.map(dish => {
     if (dish?.id && dish?.status === 'ordered') {
@@ -55,11 +53,12 @@ const Basket = () => {
     }
   }) : ''
 
+  console.log(dishesList)
   return (
     <div className="basket">
       <div className="basket__icons">
         <i onClick={() => updateAction(ActionTypes.BASE)} className="fas fa-arrow-circle-left arrow" />
-        {dishes.length > 0 && <i onClick={() => updateAction(ActionTypes.ORDER_STATUS)} className="fas fa-clipboard-list list"/>}
+        {dishess.length > 0 && <i onClick={() => updateAction(ActionTypes.ORDER_STATUS)} className="fas fa-clipboard-list list"/>}
       </div>
       <div className="basketContainer">
         {dishesList.length > 0 ? dishesList : <h1 className="basket__message">You haven't added any dish to the order :(</h1>}
