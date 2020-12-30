021c1e1ab9315a58c4953fb20273332323114d1d
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-12-30 14:16:42 -0600

    bugfix(menu-dishes): change input to add file directly in form instead url

--- src/components/AddItem/index.jsx
+++ src/components/AddItem/index.jsx
@@ -9,27 +9,6 @@ const AddItem = () => {
   const [dish, setDish] = useState(dishSelected)
   const [categories, setCategories] = useState([])
 
-  const handleSubmit = () => {
-    if (actionLayout === ActionTypes.DISH_ADD) {
-      dish.price = Number(dish.price);
-      dish.categoryId = Number(categorySelected.id);
-      dishService.create(dish)
-        .then(() => updateModalDisplay('ADD_DISH'))
-    } else {
-      dish.dishId = dish.id;
-      dishService.update(dish)
-        .then((res) =>  updateModalDisplay('UPDATED_DISH'))
-    }
-  }
-
-  const handleChange = (e) => {
-    const newDish = {
-      ...dish,
-      [e.target.id]: e.target.value
-    }
-    setDish(newDish)
-  }
-
   useEffect(()=> {
     categoryService.getAll()
       .then(data => {
@@ -38,17 +17,82 @@ const AddItem = () => {
     setDish(dishSelected)
   }, [dishSelected])
 
-  const categoryOptions = categories.map(category => <option value={category.id} key={category.id}  selected={ dish.categoryId==category.id }  >{category.name}</option>)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (actionLayout === ActionTypes.DISH_ADD) {
+      createDish();
+    } else {
+      updateDish();
+    }
+  }
+
+  const createDish = () => {
+    dish.price = Number(dish.price);
+    dish.categoryId = Number(categorySelected.id);
+    dishService.create(dish)
+      .then((res) => dishService.updateImage({dishId: res.id, image: dish.image}))
+      .then(()=> updateModalDisplay('ADD_DISH'));
+  }
+
+  const updateDish = () => {
+    dish.dishId = dish.id;
+    dishService.update(dish)
+      .then((res) =>  {
+        //if  user add new image then update image if not only return updated dish
+        if (dish.image) {
+          return dishService.updateImage({dishId: res.id, image: dish.image});
+        } else {
+          return res;
+        }
+      })
+      .then(()=> updateModalDisplay('UPDATED_DISH'))
+  }
+  /**
+   * Update input value in state
+   * @param {Event} e form event
+   */
+  const handleChange = (e) => {
+    const newDish = {
+      ...dish,
+      [e.target.id]: e.target.value
+    }
+    setDish(newDish)
+  }
+
+  /**
+   * Handle the value of input file to show image when select and prepare form to submit
+   * @param {Event} e input file event
+   */
+  const handleImage = (e) => {
+    let file = e.target.files[0];
+    const customEvent = {
+      target: {
+        id: 'image',
+        value: file
+      }
+    }
+    handleChange(customEvent);
+  }
+
+  /**
+   * Choose what image should be displayed the default or new image
+   */
+  const handleDisplayImage = () => {
+    if (dish.imageUrl && !dish.image) {
+      return <img src={dish.imageUrl} alt=""/>;
+    } else if (dish.imageUrl){
+      return <img src={URL.createObjectURL(dish.image)} alt=""/>;
+    }
+  }
 
   return (
     <div className="addItem">
-      <form key={dish.id} action="" className="addItem__form" onSubmit={()=> handleSubmit()}>
+      <form key={dish.id} action="" className="addItem__form" onSubmit={(e)=> handleSubmit(e)}>
         <i onClick={() => updateAction(ActionTypes.BASE)} className="fas fa-arrow-circle-left arrow" />
-        {dish.imageUrl &&
           <div className="addItem__form--image">
-            <img src={dish.imageUrl} alt=""/>
+            { handleDisplayImage() }
           </div>
-        }
         <div className="addItem__form--group">
           <label htmlFor="name">Add title</label>
           <input type="text" id="name" defaultValue={dish.name} onChange={(e)=>handleChange(e)} />
@@ -62,13 +106,13 @@ const AddItem = () => {
           <input type="text" id="price" defaultValue={dish.price} onChange={(e)=>handleChange(e)} />
         </div>
         <div className="addItem__form--group">
-          <label htmlFor="imageUrl">Image URL</label>
-          <input type="text" id="imageUrl" defaultValue={dish.imageUrl} onChange={(e)=>handleChange(e)} />
+          <label htmlFor="imageUrl">Image</label>
+          <input type="file" id="imageUrl" onChange={(e)=>handleImage(e)} />
         </div>
         <div className="addItem__form--group">
           <label htmlFor="categoryId">Categoría</label>
-          <select type="text" id="categoryId" disabled>
-            <option value={categorySelected.id} selected>{categorySelected.name}</option>
+          <select type="text" id="categoryId" disabled defaultValue={categorySelected.id}>
+            <option value={categorySelected.id}>{categorySelected.name}</option>
           </select>
         </div>
         <input type="submit" value="Send" className="addItem__form--submit" />
